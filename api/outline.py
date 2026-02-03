931c10932857d2081107199b0b2606b341d69385
PBNadar <36203852+PBNadar@users.noreply.github.com>
2026-02-03 18:52:49 +0530

    Update outline.py

--- api/outline.py
+++ api/outline.py
@@ -1,27 +1,20 @@
 from fastapi import FastAPI, Query
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
+from mangum import Mangum
 import requests
 from bs4 import BeautifulSoup
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["GET"],
-    allow_headers=["*"],
-)
-
 @app.get("/")
-def get_outline(country: str = Query(...)):
+def outline(country: str = Query(...)):
     url = f"https://en.wikipedia.org/wiki/{country.replace(' ', '_')}"
     headers = {"User-Agent": "GlobalEduBot/1.0"}
 
     html = requests.get(url, headers=headers).text
     soup = BeautifulSoup(html, "html.parser")
 
-    headings = soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])
+    headings = soup.find_all(["h1","h2","h3","h4","h5","h6"])
 
     outline = ["## Contents\n"]
     for h in headings:
@@ -29,3 +22,5 @@ def get_outline(country: str = Query(...)):
         outline.append("#" * level + " " + h.get_text(strip=True))
 
     return "\n".join(outline)
+
+handler = Mangum(app)
